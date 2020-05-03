b802a5d94dea8b9d9dee1433047852265181009f
aviral243 <aviralgangwar24@gmail.com>
2020-05-03 12:11:45 +0530

    using variable k and accuracy reporting

--- plinko/score.js
+++ plinko/score.js
@@ -1,5 +1,4 @@
 const outputs = [];
-const k = 3;
 
 const onScoreUpdate = (dropPosition, bounciness, size, bucketLabel) => {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
@@ -9,17 +8,20 @@ const runAnalysis = () => {
   const testSetSize = 10;
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
-  let numberCorrect = 0;
-  for (let i = 0; i < testSet.length; i++) {
-    const bucket = knn(trainingSet, testSet[i][0]);
-    if (bucket === testSet[i][3]) {
-      numberCorrect++;
-    }
-  }
-  console.log("Accuracy: ", numberCorrect / testSetSize);
+  _.range(1, 15).forEach((k) => {
+    const accuracy = _.chain(testSet)
+      .filter((testPoint) => {
+        return knn(trainingSet, testPoint[0], k) === testPoint[3];
+      })
+      .size()
+      .divide(testSetSize)
+      .value();
+
+    console.log(accuracy);
+  });
 };
 
-const knn = (data, point) => {
+const knn = (data, point, k) => {
   return _.chain(data)
     .map((row) => [distance(row[0], point), row[3]])
     .sortBy((row) => row[0])
